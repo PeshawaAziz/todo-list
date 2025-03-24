8bca505d7b10dd8b5d1e2e953dafa68bb67a39e1
PeshawaAziz <modern3p2017@gmail.com>
2025-03-24 12:49:33 +0330

    chore: add initial files for the database

---
+++ db/Database.java
@@ -0,0 +1,23 @@
+package db;
+
+import java.util.ArrayList;
+
+public class Database {
+    private ArrayList<Entity> entities;
+
+    public void add(Entity e) {
+
+    }
+
+    public void get(int id) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+
+    public void update(Entity e) {
+
+    }
+}
