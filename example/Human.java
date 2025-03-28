2215eb1045fa1585c3c5dd97e63d68e80bd275e9
PeshawaAziz <modern3p2017@gmail.com>
2025-03-28 18:06:42 +0330

    feat: add age field to the Human class and update constructor and clone method accordingly

--- example/Human.java
+++ example/Human.java
@@ -4,8 +4,15 @@ import db.Entity;
 
 public class Human extends Entity {
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public Human clone() {
+        return (Human) super.clone();
     }
 }
