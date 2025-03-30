a0ed1335d34baf0157e1ba9de37eee49d3194d53
PeshawaAziz <modern3p2017@gmail.com>
2025-03-30 19:48:58 +0330

    refactor: seperate name and age validation for better exception handling

--- example/HumanValidator.java
+++ example/HumanValidator.java
@@ -8,8 +8,12 @@ public class HumanValidator implements Validator {
     @Override
     public void validate(Entity entity) throws InvalidEntityException {
         if (entity instanceof Human human) {
-            if (human.name == null || human.name.isEmpty() || human.age < 0)
-                throw new InvalidEntityException("The provided Human entity is invalid.");
+            if (human.name == null || human.name.isEmpty())
+                throw new InvalidEntityException(
+                        "Entity with id=" + entity.id + " is invalid. (Human with null or empty name)");
+            else if (human.age < 0)
+                throw new InvalidEntityException(
+                        "Entity with id=" + entity.id + " is invalid. (Human with negative age)");
         } else {
             throw new IllegalArgumentException("The provided entity isn't an instance of Human.");
         }
