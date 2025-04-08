6ed0c8e6ef6100e884c6761dabdaf9bf8fa04b37
PeshawaAziz <modern3p2017@gmail.com>
2025-04-08 20:18:45 +0330

    feat: initialize input handling structure in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] input;
+
+        do {
+            input = in.nextLine().split(" ");
+
+            switch (input[0]) {
+                case "add":
+                    switch (input[1]) {
+                        case "task" -> {
+                        }
+                        case "step" -> {
+                        }
+                        default -> throw new AssertionError();
+                    }
+                    break;
+
+                case "delete":
+                    break;
+
+                case "update":
+                    switch (input[1]) {
+                        case "task" -> {
+                        }
+                        case "step" -> {
+                        }
+                        default -> throw new AssertionError();
+                    }
+                    break;
+
+                case "get":
+                    switch (input[1]) {
+                        case "task-by-id" -> {
+                        }
+                        case "all-tasks" -> {
+                        }
+                        case "incomplete-tasks" -> {
+                        }
+                        default -> throw new AssertionError();
+                    }
+                    break;
+
+                default:
+                    throw new AssertionError();
+            }
+        } while (!input[0].equals("exit"));
     }
 }
