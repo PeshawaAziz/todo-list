34f9bc2468a8211f479d57727399ab3b45a75249
PeshawaAziz <modern3p2017@gmail.com>
2025-04-11 00:33:11 +0330

    feat: implement add and delete commands without exception handling

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import db.Database;
+import java.util.Date;
 import java.util.Scanner;
+import todo.service.StepService;
+import todo.service.TaskService;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +13,60 @@ public class Main {
             input = in.nextLine().split(" ");
 
             switch (input[0]) {
-                case "add":
+                case "add" -> {
+                    switch (input[1]) {
+                        case "task" -> {
+                            String title, description;
+                            String[] timestamp;
+                            int year, month, day;
+                            Date dueDate;
+
+                            System.out.println("Title: ");
+                            title = in.nextLine();
+
+                            System.out.println("Description: ");
+                            description = in.nextLine();
+                            timestamp = in.nextLine().split("-");
+                            year = Integer.parseInt(timestamp[0]);
+                            month = Integer.parseInt(timestamp[1]);
+                            day = Integer.parseInt(timestamp[2]);
+
+                            System.out.println("Due date (yyyy-mm-dd): ");
+                            dueDate = new Date(year, month, day);
+
+                            // TODO Exception Handling
+                            TaskService.saveTask(title, description, dueDate);
+                        }
+
+                        case "step" -> {
+                            int taskId;
+                            String title;
+
+                            System.out.println("Task ID: ");
+                            taskId = in.nextInt();
+
+                            System.out.println("Title: ");
+                            title = in.nextLine();
+
+                            // TODO Exception Handling
+                            StepService.saveStep(taskId, title);
+                        }
+
+                        default -> throw new AssertionError();
+                    }
+                }
+
+                case "delete" -> {
+                    int id;
+
+                    System.out.println("ID: ");
+                    id = in.nextInt();
+
+                    // TODO Exception Handling
+                    Database.delete(id);
+                }
+
+                case "update" -> {
                     switch (input[1]) {
                         case "task" -> {
                         }
@@ -17,22 +74,9 @@ public class Main {
                         }
                         default -> throw new AssertionError();
                     }
-                    break;
+                }
 
-                case "delete":
-                    break;
-
-                case "update":
-                    switch (input[1]) {
-                        case "task" -> {
-                        }
-                        case "step" -> {
-                        }
-                        default -> throw new AssertionError();
-                    }
-                    break;
-
-                case "get":
+                case "get" -> {
                     switch (input[1]) {
                         case "task-by-id" -> {
                         }
@@ -42,10 +86,9 @@ public class Main {
                         }
                         default -> throw new AssertionError();
                     }
-                    break;
+                }
 
-                default:
-                    throw new AssertionError();
+                default -> throw new AssertionError();
             }
         } while (!input[0].equals("exit"));
     }
