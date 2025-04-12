eca55ef54a6b9b0cfab6e42a14aedd94e8cc57fa
PeshawaAziz <modern3p2017@gmail.com>
2025-04-12 16:18:13 +0330

    feat: implement update task and step commands

--- Main.java
+++ Main.java
@@ -69,10 +69,198 @@ public class Main {
                 case "update" -> {
                     switch (input[1]) {
                         case "task" -> {
+                            int taskId;
+                            String field;
+                            int stepId;
+                            String field;
+
+                            System.out.println("ID: ");
+                            stepId = in.nextInt();
+
+                            System.out.println("Field (title, status): ");
+                            field = in.nextLine();
+
+                            System.out.println("New value: ");
+                            switch (field) {
+                                case "title" -> {
+                                    String newTitle = in.nextLine();
+
+                                    // TODO Exception Handling
+                                    StepService.updateTitle(stepId, newTitle);
+                                }
+
+                                case "status" -> {
+                                    System.out.println("New value (NotStarted, Completed): ");
+                                    String newStatus = in.nextLine();
+
+                                    switch (newStatus) {
+                                        case "NotStarted" -> {
+                                            StepService.setAsCompleted(stepId);
+                                        }
+
+                                        case "Completed" -> {
+                                            StepService.setAsCompleted(stepId);
+                                        }
+
+                                        default -> {
+                                            System.out.println("Invalid input.");
+                                        }
+                                    }
+
+                                }
+
+                                default -> {
+                                    System.out.println("Invalid input.");
+                            int stepId;
+                            String field;
+
+                            System.out.println("ID: ");
+                            stepId = in.nextInt();
+
+                            System.out.println("Field (title, status): ");
+                            field = in.nextLine();
+
+                            System.out.println("New value: ");
+                            switch (field) {
+                                case "title" -> {
+                                    String newTitle = in.nextLine();
+
+                                    // TODO Exception Handling
+                                    StepService.updateTitle(stepId, newTitle);
+                                }
+
+                                case "status" -> {
+                                    System.out.println("New value (NotStarted, Completed): ");
+                                    String newStatus = in.nextLine();
+
+                                    switch (newStatus) {
+                                        case "NotStarted" -> {
+                                            StepService.setAsCompleted(stepId);
+                                        }
+
+                                        case "Completed" -> {
+                                            StepService.setAsCompleted(stepId);
+                                        }
+
+                                        default -> {
+                                            System.out.println("Invalid input.");
+                                        }
+                                    }
+
+                                }
+
+                                default -> {
+                                    System.out.println("Invalid input.");
+
+                            System.out.println("ID: ");
+                            taskId = in.nextInt();
+
+                            System.out.println("Field (title, description, status, due-date): ");
+                            field = in.nextLine();
+
+                            switch (field) {
+                                case "title" -> {
+                                    System.out.println("New value: ");
+                                    String newTitle = in.nextLine();
+
+                                    // TODO Exception Handling
+                                    TaskService.updateTitle(taskId, newTitle);
+                                }
+
+                                case "description" -> {
+                                    System.out.println("New value: ");
+                                    String newDescription = in.nextLine();
+
+                                    // TODO Exception Handling
+                                    TaskService.updateDescription(taskId, newDescription);
+                                }
+
+                                case "status" -> {
+                                    System.out.println("New value (NotStarted, InProgress, Completed): ");
+                                    String newStatus = in.nextLine();
+
+                                    switch (newStatus) {
+                                        case "NotStarted" -> {
+                                            TaskService.setAsCompleted(taskId);
+                                        }
+
+                                        case "InProgress" -> {
+                                            TaskService.setAsInProgress(taskId);
+                                        }
+
+                                        case "Completed" -> {
+                                            TaskService.setAsCompleted(taskId);
+                                        }
+
+                                        default -> {
+                                            System.out.println("Invalid input.");
+                                        }
+                                    }
+
+                                }
+
+                                case "due-date" -> {
+                                    System.out.println("New value: ");
+                                    Date newDueDate = getDate(in.nextLine());
+
+                                    // TODO Exception Handling
+                                    TaskService.updateDueDate(taskId, newDueDate);
+                                }
+
+                                default -> {
+                                    System.out.println("Invalid input.");
+                                }
+                            }
                         }
+
                         case "step" -> {
+                            int stepId;
+                            String field;
+
+                            System.out.println("ID: ");
+                            stepId = in.nextInt();
+
+                            System.out.println("Field (title, status): ");
+                            field = in.nextLine();
+
+                            System.out.println("New value: ");
+                            switch (field) {
+                                case "title" -> {
+                                    String newTitle = in.nextLine();
+
+                                    // TODO Exception Handling
+                                    StepService.updateTitle(stepId, newTitle);
+                                }
+
+                                case "status" -> {
+                                    System.out.println("New value (NotStarted, Completed): ");
+                                    String newStatus = in.nextLine();
+
+                                    switch (newStatus) {
+                                        case "NotStarted" -> {
+                                            StepService.setAsCompleted(stepId);
+                                        }
+
+                                        case "Completed" -> {
+                                            StepService.setAsCompleted(stepId);
+                                        }
+
+                                        default -> {
+                                            System.out.println("Invalid input.");
+                                        }
+                                    }
+
+                                }
+
+                                default -> {
+                                    System.out.println("Invalid input.");
+                                }
+                            }
                         }
-                        default -> throw new AssertionError();
+
+                        default -> {
+                            System.out.println("Invalid input.");
+                        }
                     }
                 }
 
